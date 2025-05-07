8e7a2e8fad1cfa25d23c98681c04e8061f15a00a
NikitaKx9 <nikitakx9@gmail.com>
2025-05-07 20:47:45 +0300

    add tablet styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/style.css';
 import './styles/main.scss'
 import './styles/button.scss'
 import './styles/mobile.scss'
+import './styles/tablet.scss'
 
 import './js/app';
 import './js/insertSVG';
