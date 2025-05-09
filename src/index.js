4bfec7772a34591555a1024d565a7fde78573111
NikitaKx9 <nikitakx9@gmail.com>
2025-05-09 21:01:13 +0300

    add desktop classes, change import scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/style.css';
 
 import './styles/main.scss'
-import './styles/button.scss'
+import './styles/videoButton.scss'
 import './styles/mobile.scss'
 import './styles/tablet.scss'
+import './styles/desktop.scss'
 
 import './js/app';
 import './js/insertSVG';
