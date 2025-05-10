ad047b5b4322a32b91bffa09fb0f0a078075706d
NikitaKx9 <nikitakx9@gmail.com>
2025-05-10 11:30:56 +0300

    add form

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './styles/videoButton.scss'
 import './styles/mobile.scss'
 import './styles/tablet.scss'
 import './styles/desktop.scss'
+import './styles/form.scss'
 
 import './js/app';
 import './js/insertSVG';
