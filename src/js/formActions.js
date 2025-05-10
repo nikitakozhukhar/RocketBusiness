4c8a50cfdf9e79a9ab42316e344ef35f5f388080
NikitaKx9 <nikitakx9@gmail.com>
2025-05-10 19:23:54 +0300

    update form styles and actions

--- src/js/formActions.js
+++ src/js/formActions.js
@@ -25,6 +25,7 @@ function closeForm() {
 function disabledDelay(delay) {
   setTimeout(() => {
     formBtn.classList.remove('btn-disabled');
+    formBtn.removeAttribute('disabled')
   }, delay)
 } 
 
@@ -32,39 +33,42 @@ function disabledDelay(delay) {
 function formInputValidation() {
   const errorMessageName = document.querySelector('.name_input-error');
   const errorMessagePhone = document.querySelector('.phone_input-error');
+  const personalData = document.querySelector('.form_footer_personal')
 
+  const nameValidationSсhema = /^[A-Za-zА-Яа-яЁё][A-Za-zА-Яа-яЁё0-9]*$/
   let isValidName = false;
   let isValidPhone = false;
+  let isCheckedPersonalData = personalDataField.checked;
 
-  if (nameField.value.length === 0) {
+  if (nameField.value.length <= 2 && !nameValidationSсhema.test(nameField.value)) {
     errorMessageName.style.display = 'block';
     formBtn.classList.add('btn-disabled');
     nameField.classList.add('error');
-     disabledDelay(2000)
+    formBtn.setAttribute('disabled', true)
+    disabledDelay(2000)
   } else {
     errorMessageName.style.display = 'none';
     nameField.classList.remove('error');
     isValidName = true;
-   
   }
 
 
-  if (phoneField.value.length < 12 && phoneField.value !== Number) {
+  if (phoneField.value.trim().length < 12 && phoneField.value !== Number) {
     errorMessagePhone.style.display = 'block';
     formBtn.classList.add('btn-disabled');
     phoneField.classList.add('error');
+    formBtn.setAttribute('disabled', true)
     disabledDelay(2000)
   } else {
     errorMessagePhone.style.display = 'none';
     phoneField.classList.remove('error');
     isValidPhone = true;
-    
   }
 
-  console.log(personalDataField.value)
+  !isCheckedPersonalData ? personalData.classList.add('error_text') : personalData.classList.remove('error_text');
 
   return {
-    isValidName, isValidPhone
+    isValidName, isValidPhone, isCheckedPersonalData
   }
 
 }
@@ -72,7 +76,7 @@ function formInputValidation() {
 function submiteForm(e) {
   e.preventDefault();
 
-  const { isValidName, isValidPhone } = formInputValidation();
+  const { isValidName, isValidPhone, isCheckedPersonalData } = formInputValidation();
 
   if (!isValidName) {
     console.error('Не введено имя пользователя')
@@ -82,21 +86,17 @@ function submiteForm(e) {
     console.error('Введен не корректный номер телефона')
   }
 
-  if (isValidName && isValidPhone) {
+  if (isValidName && isValidPhone && isCheckedPersonalData) {
     const data = new FormData();
 
     data.name = nameField.value;
     data.phone = phoneField.value;
-
-
-    console.log(data)
+    data.personal = personalDataField.checked;
+    closeForm();
+    nameField.value = '';
+    phoneField.value = '';
+    personalDataField.checked = false;
   }
-
-  // formInputValidation();
-  // const data = new FormData();
-
-  // data.name = nameField.value;
-  // data.phone = phoneField.value;
 
 }
 
